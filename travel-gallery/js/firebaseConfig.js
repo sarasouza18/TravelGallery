b9913c35e9bb621b699c1487d8b0a5fe094d6adc
Sara Souza <sara.souza.9@hotmail.com>
2025-11-08 10:36:20 -0300

    Delete travel-gallery directory

--- travel-gallery/js/firebaseConfig.js
+++
@@ -1,3 +0,0 @@
-// Configure aqui para usar Firebase Realtime Database (opcional)
-// Se não configurar, o app usa LocalStorage como fallback.
-export const FIREBASE_DB_URL = ""; // ex.: https://SEU-PROJETO-default-rtdb.firebaseio.com/items
